0475096bbd935b6d394a96f97d771fa6ee3ff488
Dylan <73333005+DylanH2009@users.noreply.github.com>
2021-11-23 22:49:45 +0000

    .

--- ping.bat
+++ ping.bat
@@ -1,3 +1,18 @@
 @echo off
 
 echo Welcome! This program will check if a website or server is up.
+echo
+echo Shortcuts:
+echo 'facebook' for facebook.com
+echo 'twitter' for twitter.com
+echo 'instagram' for instagram.com
+echo 'youtube' for youtube.com
+echo 'reddit' for reddit.com 
+
+echo -------------------------------------------------------------
+echo Type '1' to check a website or '2' to check an I.P. address.
+echo -------------------------------------------------------------
+
+%type% = Enter selection: 
+ if %type% = 1 (goto WEBSITECHECK)
+ if %type% = 2 (goto IPCHECK)
