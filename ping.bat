7816bed258f299883e864f94cf93be86b2686801
Dylan <73333005+DylanH2009@users.noreply.github.com>
2021-11-24 17:26:34 +0000

    .

--- ping.bat
+++ ping.bat
@@ -15,8 +15,8 @@ echo -------------------------------------------------------------
 
 set type=
 set /p %type% = Enter selection: 
- if %type% = 1 (GOTO WEBSITECHECK)
- if %type% = 2 (GOTO IPCHECK)
+ if %type% = 1 GOTO WEBSITECHECK
+ if %type% = 2 GOTO IPCHECK
 
  pause
 
@@ -27,17 +27,17 @@ set /p %site% = Enter URL or shortcut:
 
 REM shortcuts
 
-if %site% = fb (ping facebook.com)
-if %site% = t (ping twitter.com)
-if %site% = insta (ping instagram.com)
-if %site% = yt (ping youtube.com)
-if %site% = r (ping reddit.com)
+if %site% = fb ping facebook.com
+if %site% = t ping twitter.com
+if %site% = insta ping instagram.com
+if %site% = yt ping youtube.com
+if %site% = r ping reddit.com
 
-if %site% !==! fb (GOTO OTHERSITE)
-if %site% !==! t (GOTO OTHERSITE)
-if %site% !==! insta (GOTO OTHERSITE)
-if %site% !==! yt (GOTO OTHERSITE)
-if %site% !==! r (GOTO OTHERSITE)
+if %site% !==! fb GOTO OTHERSITE
+if %site% !==! t GOTO OTHERSITE
+if %site% !==! insta GOTO OTHERSITE
+if %site% !==! yt GOTO OTHERSITE
+if %site% !==! r GOTO OTHERSITE
 
 :OTHERSITE
 
